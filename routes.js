d29026d88d0280064e85c93158027f58faebcf1f
davidecarizzoni <davide.carizzoni@runelab.it>
2021-08-17 00:04:50 +0200

    feat(products): crud

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Post = require('./src/models/Post')
+const Product = require('./src/models/Products')
 const router = express.Router()
 
 //POSTS
@@ -43,8 +44,56 @@ router.delete('/posts/:id', async (req, res) => {
         res.status(204).send()
     } catch {
         res.status(404)
+        res.send({ error: `Post doesn't exist!` })
+    }
+})
+
+//PRODUCTS
+router.get('/products', async (req, res) => {
+    const products = await Product.find().exec()
+    res.send(products)
+})
+router.post('/products', async (req, res) => {
+    const product = new Product({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price
+    })
+    await product.save()
+    res.send(product)
+})
+router.get('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findById( req.params.id )
+        res.send(product)
+    } catch {
+        res.status(404)
         res.send({ error: "Post doesn't exist!" })
     }
 })
+router.put('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findOne({ _id: req.params.id })
+
+        product.set(req.body);
+
+        await product.save()
+        res.send(product)
+    } catch {
+        res.status(404)
+        res.send({ error: "Products doesn't exist!" })
+    }
+})
+router.delete('/products/:id', async (req, res) => {
+    try {
+        await Product.deleteOne({_id: req.params.id})
+        res.status(200).send()
+    }catch {
+        res.status(404)
+        res.send({error: `Products doesn't exist!`})
+    }
+})
+
+
 
 module.exports = router
