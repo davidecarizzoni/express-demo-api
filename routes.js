59105bdfba982950048549dc5c64f00480ab0271
davidecarizzoni <davide.carizzoni@runelab.it>
2021-08-16 23:10:16 +0200

    feat (posts): get by id

--- routes.js
+++ routes.js
@@ -1,20 +1,30 @@
-const express = require("express")
+const express = require('express')
 const Post = require('./src/models/Post')
 const router = express.Router()
 
-// Get all posts
-router.get("/posts", async (req, res) => {
-    const posts = await Post.find()
+//POSTS
+router.get('/posts', async (req, res) => {
+    const posts = await Post.find().exec()
     res.send(posts)
 })
 
-router.post("/posts", async (req, res) => {
+router.post('/posts', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content,
     })
     await post.save()
     res.send(post)
+})
+
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const post = await Post.findById( req.params.id )
+        res.send(post)
+    } catch {
+        res.status(404)
+        res.send({ error: "Post doesn't exist!" })
+    }
 })
 
 module.exports = router
