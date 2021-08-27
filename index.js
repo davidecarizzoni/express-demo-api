8094a7b5cb53f59690b94429f4ae3fb5d832f3c4
davidecarizzoni <davide.carizzoni@runelab.it>
2021-08-27 21:04:36 +0200

    chore: dotenv

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express')
+require('dotenv').config();
 const routes = require('./routes')
-const mongoAtlasUri ='mongodb+srv://admin:YJXghvdgNqG4vgZm@cluster0.rha3y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
 const mongoose = require('mongoose')
 
 // Connect to MongoDB database
 mongoose
-    .connect(mongoAtlasUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         const app = express()
         app.use(express.json())
         app.use("/api", routes)
 
-        app.listen(3000, () => {
+        app.listen(process.env.PORT, () => {
             console.log("Server has started!")
         })
     })
