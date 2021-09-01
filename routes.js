3851f6c30bc0b218d97fb598161e765f757743fc
davidecarizzoni <davide.carizzoni@runelab.it>
2021-09-01 10:03:34 +0200

    feat(reservations): crud works. Clean code and unused entity

--- routes.js
+++ routes.js
@@ -1,109 +1,7 @@
 const express = require('express')
-const Post = require('./src/models/Post')
-const Product = require('./src/models/Products')
 const Reservations = require('./src/models/Reservation')
 const router = express.Router()
 
-//POSTS
-router.get('/posts', async (req, res) => {
-    const posts = await Post.find().exec()
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(posts)
-})
-router.post('/posts', async (req, res) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    })
-    await post.save()
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(post)
-})
-router.get('/posts/:id', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-        const post = await Post.findById( req.params.id )
-        res.send(post)
-    } catch {
-        res.status(404)
-        res.send({ error: "Post doesn't exist!" })
-    }
-})
-router.put("/posts/:id", async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-        const post = await Post.findOne({ _id: req.params.id })
-
-        post.set(req.body);
-
-        await post.save()
-        res.send(post)
-    } catch {
-        res.status(404)
-        res.send({ error: "Post doesn't exist!" })
-    }
-})
-router.delete('/posts/:id', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-        await Post.deleteOne({ _id: req.params.id })
-        res.status(204).send()
-    } catch {
-        res.status(404)
-        res.send({ error: `Post doesn't exist!` })
-    }
-})
-
-//PRODUCTS
-router.get('/products', async (req, res) => {
-    const products = await Product.find().exec()
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(products)
-})
-router.post('/products', async (req, res) => {
-    const product = new Product({
-        title: req.body.title,
-        description: req.body.description,
-        price: req.body.price
-    })
-    await product.save()
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(product)
-})
-router.get('/products/:id', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-        const product = await Product.findById( req.params.id )
-        res.send(product)
-    } catch {
-        res.status(404)
-        res.send({ error: "Post doesn't exist!" })
-    }
-})
-router.put('/products/:id', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-        const product = await Product.findOne({ _id: req.params.id })
-
-        product.set(req.body);
-
-        await product.save()
-        res.send(product)
-    } catch {
-        res.status(404)
-        res.send({ error: "Products doesn't exist!" })
-    }
-})
-router.delete('/products/:id', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-        await Product.deleteOne({_id: req.params.id})
-        res.status(200).send()
-    } catch {
-        res.status(404)
-        res.send({error: `Products doesn't exist!`})
-    }
-})
 
 //RESERVATIONS
 router.post('/reservations', async (req, res) => {
@@ -131,6 +29,31 @@ router.get('/reservations/:id', async (req, res) => {
     } catch {
         res.status(404)
         res.send({ error: "Reservation doesn't exist!" })
+    }
+})
+
+router.put("/reservations/:id", async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    try {
+        const reservation = await Reservations.findOne({ _id: req.params.id })
+
+        reservation.set(req.body);
+
+        await reservation.save()
+        res.send(reservation)
+    } catch {
+        res.status(404)
+        res.send({ error: "Post doesn't exist!" })
+    }
+})
+router.delete('/reservations/:id', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    try {
+        await Reservations.deleteOne({ _id: req.params.id })
+        res.status(204).send()
+    } catch {
+        res.status(404)
+        res.send({ error: `Reservations doesn't exist!` })
     }
 })
 
