289c785a34b2e7aceb226b5a97aa71fc8e5b34a7
davidecarizzoni <davide.carizzoni@runelab.it>
2021-09-05 14:55:45 +0200

    fix(posts): put

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -26,8 +26,11 @@ router.post('/', async (req, res) => {
 
 router.put("/:id", async (req, res) => {
     try {
-        const reservation = await Post.updateOne({ _id: req.params.id }, {$set: req.body})
-        res.json(reservation)
+       const post = await Post.findOne({_id: req.params.id})
+       post.set(req.body)
+
+       await post.save
+       res.json(post)
     } catch {
         res.status(404)
         res.send({ error: "Post doesn't exist!" })
